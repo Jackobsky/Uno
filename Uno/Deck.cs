f02ea0bbda43f629197fab50894622bab98819ec
filip <filip@DESKTOP-AAELOSV>
2025-09-01 13:44:49 +0200

    New variable deck haha dick

---
+++ Uno/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno
+{
+    internal class Deck
+    {
+        public Deck() { }
+
+        private List<Card> cards = new List<Card>();
+
+        public void AddDeck()
+        {
+            
+        }
+    }
+}
