a197d54da84d232954d4434c0965d50cc01dec9a
Jackobsky <Jackobsky@DESKTOP-AFO0VGV>
2025-09-15 13:07:00 +0200

    Added Give card functionality, draw card etc

--- Uno/GameLogic.cs
+++ Uno/GameLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Uno
 {
@@ -8,6 +9,7 @@ namespace Uno
         public int currentPlayerIndex = 0;
         private List<Player> players = new List<Player>();
         private Deck deck;
+        private List<Card> discardPile = new List<Card>();
 
         public GameLogic(Deck deck)
         {
@@ -26,7 +28,7 @@ namespace Uno
             Console.WriteLine("1. Start Game");
             Console.WriteLine("2. Exit");
 
-            int choice = int.Parse(Console.ReadLine());
+            int choice = int.Parse(Console.ReadLine() ?? "2");
             if (choice == 1)
             {
                 Console.Clear();
@@ -38,17 +40,16 @@ namespace Uno
                     deck.GiveCards(player.Hand);
                 }
 
-                ShowGameScreen();
-            }
-            else
-            {
-                Console.Clear();
+                // Lägg ut första kortet på bordet
+                discardPile.Add(deck.DrawCard());
+
+                PlayTurns();
             }
         }
 
         static List<Player> ChoosePlayerNames()
         {
-            int numPlayers = 2; // TODO: Let the user choose later
+            int numPlayers = 2; // kan ändras senare
             List<Player> players = new List<Player>();
 
             for (int i = 1; i <= numPlayers; i++)
@@ -56,9 +57,7 @@ namespace Uno
                 Console.WriteLine($"Enter name for Player {i}:");
                 string name = Console.ReadLine();
                 if (string.IsNullOrWhiteSpace(name))
-                {
                     name = "Player" + i;
-                }
 
                 players.Add(new Player(name));
                 Console.Clear();
@@ -67,9 +66,69 @@ namespace Uno
             return players;
         }
 
+        public void PlayTurns()
+        {
+            while (true)
+            {
+                Console.Clear();
+                ShowGameScreen();
+                var currentPlayer = players[currentPlayerIndex];
+
+                Console.WriteLine("\nYour hand:");
+                for (int i = 0; i < currentPlayer.Hand.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {currentPlayer.Hand[i]}");
+                }
+
+                Console.WriteLine("\nChoose a card to play (number) or 0 to draw:");
+                int choice = int.Parse(Console.ReadLine() ?? "0");
+
+                if (choice == 0)
+                {
+                    // Dra kort
+                    currentPlayer.Hand.Add(deck.DrawCard());
+                    Console.WriteLine("You drew a card. Press any key to continue.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Card selectedCard = currentPlayer.Hand[choice - 1];
+                    if (CanPlayCard(selectedCard))
+                    {
+                        discardPile.Add(selectedCard);
+                        currentPlayer.Hand.RemoveAt(choice - 1);
+                        Console.WriteLine($"You played {selectedCard}. Press any key to continue.");
+                        Console.ReadKey();
+                        NextPlayer();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You cannot play that card. Press any key to try again.");
+                        Console.ReadKey();
+                    }
+                }
+
+                if (currentPlayer.Hand.Count == 0)
+                {
+                    Console.WriteLine($"{currentPlayer.Name} wins!");
+                    break;
+                }
+            }
+        }
+
+        private bool CanPlayCard(Card card)
+        {
+            Card topCard = discardPile.Last();
+            return card.color == topCard.color || card.value == topCard.value || card.color == "Black";
+        }
+
+        private void NextPlayer()
+        {
+            currentPlayerIndex = (currentPlayerIndex + 1) % players.Count;
+        }
+
         public void ShowGameScreen()
         {
-            Console.Clear();
             Console.WriteLine("Current game state:");
             Console.WriteLine("-------------------");
 
@@ -79,6 +138,8 @@ namespace Uno
                 string turnMarker = (i == currentPlayerIndex) ? " <--- Current turn" : "";
                 Console.WriteLine($"{player.Name} | Cards left: {player.Hand.Count}{turnMarker}");
             }
+
+            Console.WriteLine($"\nLast played card: {discardPile.Last()}");
         }
     }
 }
